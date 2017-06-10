4c0419731428624b934336dc3f69c04fbc6f9aee
lalogaribo <lalo.garibo90@gmail.com>
2017-06-10 16:44:13 -0500

    implemented custom scoope

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,7 @@ Rails.application.routes.draw do
 
   get 'about-me', to: 'pages#about'
   get 'contact', to: 'pages#contact'
+  get 'angular-items', to: 'portfolios#angular'
 
 
   resources :blogs do

5d8bfc66da317ef0d7913b39f13659854b7bff91
lalogaribo <lalo.garibo90@gmail.com>
2017-06-28 19:43:10 -0500

    Added image default

--- app/controllers/portfolios_controller.rb
+++ app/controllers/portfolios_controller.rb
@@ -5,13 +5,13 @@ class PortfoliosController < ApplicationController
   def angular
     @angular_portfolio_items = Portfolio.angular
   end
+  
   def new
     @portfolio_items = Portfolio.new
   end
 
   def create
     @portfolio_items = Portfolio.new(params.require(:portfolio).permit(:title, :subtitle, :body))
-
     respond_to do |format|
       if @portfolio_items.save
         format.html { redirect_to portfolios_path, notice: 'Your Portfolio Item was created' }

a917c1c5ecdf36e66d85a2dc1aaaa6ddd2ad91da
lalogaribo <lalo.garibo90@gmail.com>
2017-07-30 16:50:52 -0500

    Implemented partials for navigation, footer, and masthead for portfolio layout

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -11,7 +11,7 @@ module ApplicationHelper
   def source_helper(layout_name)
     if session[:source]
       greeting = "Thanks for visiting me from #{session[:source]} and you are on the #{layout_name} layout"
-      content_tag(:p, greeting, class: 'source-greeting')
+      content_tag(:p, greeting, class: 'source-greeting text-center')
     end
   end
 end

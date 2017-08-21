f2c861e87556e240f7a46d9f877063278cd25b08
lalogaribo <lalo.garibo90@gmail.com>
2017-08-20 20:07:42 -0500

    Updated file upload forms

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -14,6 +14,7 @@ module ApplicationHelper
       content_tag(:p, greeting, class: 'source-greeting text-center')
     end
   end
+
   def nav_items
     [
         {
@@ -38,13 +39,15 @@ module ApplicationHelper
         }
     ]
   end
+
   def nav_helper(style, tag_type)
     nav_links = ''
     nav_items.each do |item|
       nav_links << "<#{tag_type}><a href='#{item[:url]}' class='#{style} #{active? item[:url] }'>#{item[:title]}</a></#{tag_type}>"
     end
-  nav_links.html_safe
+    nav_links.html_safe
   end
+
   def active?(path)
     "active" if current_page? path
   end
